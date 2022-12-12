58ca977f4dd962ab5a44937473dd5776fb13ef5c
Austin Boyd <austinjedboyd@gmail.com>
2022-12-12 10:30:24 -0700

    Update bulkUpdateProjectRoles.groovy
    Refined script with more clear commenting and more concise examples

--- jira/scripts/bulkUpdateProjectRoles.groovy
+++ jira/scripts/bulkUpdateProjectRoles.groovy
@@ -1,9 +1,10 @@
 // update multiple project roles with group 
 
-def accountId = '123456:12345a67-bbb1-12c3-dd45-678ee99f99g0' // could also be turned into array list
-def groupName = 'Engineering Managers' // name of group in Jira
-def projectKey = ["AI", "ANP", "AUDI", "CAMP", "CPT", "APP", "PRO", "FE", "CO", "DOT", "DSML", "DLVR", "DEVX", "FRAM", "EX", "PLAT", "IN", "WF", "MLP", "MOB", "PER", "PLFR", "ANA", "AOS", "CORE", "SREIR", "TQ", "UDI"]] // an array list
-def roleName = 'Administrators'
+
+def accountId = '123456:12345a67-bbb1-12c3-dd45-678ee99f99g0' // the ID for the user adding to roles
+def groupName = 'Engineering Managers' // name of group for adding to roles
+def projectKey = ["AI", "ANP", "AUDI", "CAMP", "CPT", "APP"] // The projects that each groupName or accountId will be added to // as an array list
+def roleName = 'Administrators' // name of Role the accountId or groupName should be assigned 
 
 // loop through projectKey array
 for (key in projectKey){
@@ -11,14 +12,14 @@ for (key in projectKey){
         .asObject(Map).body
     //println key // for testing to see if key is accurate
     
-    String developersUrl = roles[roleName] // converts map into string 
+    String developersUrl = roles[roleName] // converts map into string
 
     assert developersUrl != null
     
     def result = post(developersUrl)
         .header('Content-Type', 'application/json')
         .body([
-                // use either account ID or group, account ID's can become an 
+                // use either accountId or groupName, I don't think this script can do both simultaneously
                 //user: [accountId],
                 group: [groupName]
         ])
